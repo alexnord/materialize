798b29c1a713dc6e1e38dbd11823dad74bba1d22
Alex Nordlinger <alex.nordlinger@gmail.com>
2018-09-03 16:39:37 -0700

    Added font-awesome and changed the format of the work page.

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -14,88 +14,124 @@
         <div class="projects">
 
           <b-row class="project" v-scroll-reveal="{ delay: 75, origin: 'left', distance: '10px' }">
-            <b-col xs="12" class="max-1140 mx-auto">
+            <b-col cols="12" class="max-1140 mx-auto">
               <a href="http://www.lfthebrand.com" target="_blank">
                 <b-row>
                   <b-col class="text-center">
                     <img src="../assets/img/work/lftb-showcase.jpg" class="img-fluid" alt="LF The Brand" />
                   </b-col>
                 </b-row>
-                <b-row class="mt-3">
-                  <b-col>
-                    <div class="work-client circular-black pink-font">LF The Brand</div>
+                <b-row class="project-info mx-auto mt-2 p-2">
+                  <b-col cols="12" md="4">
+                    <div class="client-name circular-black white-font">LF The Brand</div>
+                    <div class="project-type circular-book white-font">e-commerce website</div>
                   </b-col>
-                </b-row>
-                <b-row>
-                  <b-col>
-                    <div class="work-description circular-book">Frontend and backend web development services for Shopify hosted e-commerce store for major fashion retailer.</div>
+                  <b-col cols="12" md="6">
+                    <div class="project-description circular-med white-font">Frontend and backend web development services for Shopify hosted e-commerce store for major fashion retailer.</div>
                   </b-col>
+                  <b-col cols="12" md="2" class="d-none d-md-block">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </b-col>
+                  <div class="d-block d-md-none position-absolute mobile-project-link">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </div>
                 </b-row>
               </a>
             </b-col>
           </b-row>
 
           <b-row class="mt-35 project" v-scroll-reveal="{ delay: 75, origin: 'right', distance: '10px' }">
-            <b-col xs="12" class="max-1140 mx-auto">
+            <b-col cols="12" class="max-1140 mx-auto">
               <a href="http://www.carmardenim.com" target="_blank">
                 <b-row>
                   <b-col>
                     <img src="../assets/img/work/cm-showcase.jpg" class="img-fluid" alt="Carmar Denim" />
                   </b-col>
                 </b-row>
-                <b-row class="mt-2">
-                  <b-col>
-                    <div class="work-client circular-black pink-font"><strong>Carmar Denim</strong></div>
+                <b-row class="project-info mx-auto mt-2 p-2">
+                  <b-col cols="12" md="4">
+                    <div class="client-name circular-black white-font">Carmar Denim</div>
+                    <div class="project-type circular-book white-font">e-commerce website</div>
                   </b-col>
-                </b-row>
-                <b-row>
-                  <b-col>
-                    <div class="work-description circular-book">Frontend and backend web development services for Shopify hosted e-commerce store for major fashion retailer.</div>
+                  <b-col cols="12" md="6">
+                    <div class="project-description circular-med white-font">Frontend and backend web development services for Shopify hosted e-commerce store for major fashion retailer.</div>
                   </b-col>
+                  <b-col cols="12" md="2" class="d-none d-md-block">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </b-col>
+                  <div class="d-block d-md-none position-absolute mobile-project-link">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </div>
                 </b-row>
               </a>
             </b-col>
           </b-row>
 
           <b-row class="mt-35 project" v-scroll-reveal="{ delay: 75, origin: 'left', distance: '10px' }">
-            <b-col xs="12" class="max-1140 mx-auto">
+            <b-col cols="12" class="max-1140 mx-auto">
               <a href="http://www.emmaandsam.com" target="_blank">
                 <b-row>
                   <b-col>
                     <img src="../assets/img/work/es-showcase.jpg" class="img-fluid" alt="Emma and Sam" />
                   </b-col>
                 </b-row>
-                <b-row class="mt-2">
-                  <b-col>
-                    <div class="work-client circular-black pink-font"><strong>Emma and Sam</strong></div>
+                <b-row class="project-info mx-auto mt-2 p-2">
+                  <b-col cols="12" md="4">
+                    <div class="client-name circular-black white-font">Emma and Sam</div>
+                    <div class="project-type circular-book white-font">e-commerce website</div>
                   </b-col>
-                </b-row>
-                <b-row>
-                  <b-col>
-                    <div class="work-description circular-book">Frontend and backend web development services for Shopify hosted e-commerce store for major fashion retailer.</div>
+                  <b-col cols="12" md="6">
+                    <div class="project-description circular-med white-font">Frontend and backend web development services for Shopify hosted e-commerce store for major fashion retailer.</div>
                   </b-col>
+                  <b-col cols="12" md="2" class="d-none d-md-block">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </b-col>
+                  <div class="d-block d-md-none position-absolute mobile-project-link">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </div>
                 </b-row>
               </a>
             </b-col>
           </b-row>
 
           <b-row class="mt-35 mb-5 project" v-scroll-reveal="{ delay: 75, origin: 'right', distance: '10px' }">
-            <b-col xs="12" class="max-1140 mx-auto">
+            <b-col cols="12" class="max-1140 mx-auto">
               <a href="http://www.lifeofthepartypremiere.com" target="_blank">
                 <b-row>
                   <b-col>
                     <img src="../assets/img/work/lotp-showcase.jpg" class="img-fluid" alt="Life of the Party" />
                   </b-col>
                 </b-row>
-                <b-row class="mt-2">
-                  <b-col>
-                    <div class="work-client circular-black pink-font"><strong>Life of the Party Premiere</strong></div>
+                <b-row class="project-info mx-auto mt-2 p-2">
+                  <b-col cols="12" md="4">
+                    <div class="client-name circular-black white-font">Life of the Party Premiere</div>
+                    <div class="project-type circular-book white-font">responsive website</div>
                   </b-col>
-                </b-row>
-                <b-row>
-                  <b-col>
-                    <div class="work-description circular-book">Promotional website built for the soon-to-be-released Life of the Party movie. Provided custom built voting functionality for college students to win the world premiere at their college campus.</div>
+                  <b-col cols="12" md="6">
+                    <div class="project-description circular-med white-font">Promotional website built for the soon-to-be-released Life of the Party movie. Provided custom built voting functionality for college students to win the world premiere at their college campus.</div>
                   </b-col>
+                  <b-col cols="12" md="2" class="d-none d-md-block">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </b-col>
+                  <div class="d-block d-md-none position-absolute mobile-project-link">
+                    <div class="project-link circular-book white-font">
+                      <div><i class="fas fa-external-link-alt"></i></div>
+                    </div>
+                  </div>
                 </b-row>
               </a>
             </b-col>
@@ -117,18 +153,8 @@ export default {
 };
 </script>
 
-<style scoped>
-.project::after {
-  border-bottom: 1px solid rgba(0, 0, 0, .6);
-  width: 90%;
-  content: " ";
-  margin: 0 auto;
-  padding-top: 28px;
-}
-.project:last-child:after
-{
-  border-bottom: none;
-}
+<style lang="scss" scoped>
+
 .header {
   position: relative;
   margin-left: 9%;
@@ -151,12 +177,27 @@ export default {
 .projects a:hover {
   text-decoration: none;
 }
-.work-client {
+.project-info {
+  background-color: rgba(223, 0, 112, .8);
+}
+.client-name {
   font-size: 24px;
 }
-.work-description {
-  font-size: 16px;
-  max-width: 1140px;
+.project-type {
+  font-size: 18px;
+}
+.project-description {
+  font-size: 18px;
+}
+.project-link {
+  font-size: 26px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.mobile-project-link {
+  right: 23px;
 }
 
 @media (min-width: 768px) {
@@ -175,11 +216,17 @@ export default {
   .projects {
     margin-top: 50px;
   }
-  .work-client {
+  .client-name {
     font-size: 30px;
   }
-  .work-description {
+  .project-type {
     font-size: 22px;
+  }
+  .project-description {
+    font-size: 22px;
+  }
+  .project-link {
+    font-size: 32px;
   }
 }
 </style>

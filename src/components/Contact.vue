45ecad8bd487893ba868b1b92182e966a38ab09d
Alex Nordlinger <alex.nordlinger@gmail.com>
2019-08-02 13:12:21 -0700

    Updated all numbers to be Alexs.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -21,7 +21,7 @@
                       <p class="circular-book pink-font">Or reach out to us at:</p>
                       <p class="circular-book mt-2">
                         <i class="far fa-envelope pink-font"></i><a href="mailto:hello@materializelabs.com">hello@materializelabs.com</a></p>
-                      <p class="circular-book"><i class="fas fa-phone pink-font"></i>(424) 256-8872</p>
+                      <p class="circular-book"><i class="fas fa-phone pink-font"></i>(310) 699-0125</p>
                       <p class="circular-book"><i class="fab fa-facebook-messenger pink-font"></i><a href="https://m.me/materializelabs/">FB Messenger</a></p>
                     </div>
                   </div>
